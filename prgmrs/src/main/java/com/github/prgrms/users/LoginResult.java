0f5d2657b81a12b5ff011ff916261e8da67d1ed7
xeroman.k <xeroman.k@itsmart.co.kr>
2021-11-10 10:52:53 +0900

    end commit

--- prgmrs/src/main/java/com/github/prgrms/users/LoginResult.java
+++ prgmrs/src/main/java/com/github/prgrms/users/LoginResult.java
@@ -5,29 +5,29 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class LoginResult {
 
-  private final String token;
+    private final String token;
 
-  private final UserDto user;
+    private final UserDto user;
 
-  public LoginResult(String token, User user) {
-    this.token = token;
-    this.user = new UserDto(user);
-  }
+    public LoginResult(String token, User user) {
+        this.token = token;
+        this.user = new UserDto(user);
+    }
 
-  public String getToken() {
-    return token;
-  }
+    public String getToken() {
+        return token;
+    }
 
-  public UserDto getUser() {
-    return user;
-  }
+    public UserDto getUser() {
+        return user;
+    }
 
-  @Override
-  public String toString() {
-    return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
-      .append("token", token)
-      .append("user", user)
-      .toString();
-  }
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("token", token)
+                .append("user", user)
+                .toString();
+    }
 
 }
